74c3bf1becb1a92bbb9f096be0adaea64864587d
jinyongkwon <wlsdyd1178@naver.com>
2022-04-13 19:32:09 +0900

    [권진용]회원가입 중복유저네임, 중복닉네임, 비밀번호 체크 유효성 검사 JS완료

--- src/main/resources/static/js/joinForm.js
+++ src/main/resources/static/js/joinForm.js
@@ -1,21 +1,88 @@
-let checkPassword = false;
 
 $("#password-error").hide();
-$("#password2").blur(() => {
-    let password = $("#password1").val();
-    let password2 = $("#password2").val();
-    if (password != password2) {
-        $("#password-error").show();
-    } else {
-        $("#password-error").hide();
-        checkPassword = true;
-    }
+$("#username-error").hide();
+$("#nickname-error").hide();
+
+let valid = {
+    username: {
+        state: false,
+    },
+    password: {
+        state: false,
+    },
+    nickname: {
+        state: false,
+    },
+}
+
+$("#password").blur(() => {
+    passwordSameCheck();
+});
+
+$("#smae-password").blur(() => {
+    passwordSameCheck();
+});
+
+$("#username").blur(() => {
+    usernameSameCheck();
+});
+
+$("#nickname").blur(() => {
+    nicknameSameCheck();
 });
 
 function check() {
-    if (checkPassword == true) {
+    if (valid.username.state && valid.password.state &&valid.nickname.state) {
         return true;
-    } else {
+    }else{
         return false;
     }
 };
+
+async function nicknameSameCheck() {
+    let nickname = $("#username").val();
+
+    let response = await fetch(`/api/user/nickname-same-check?nickname=${nickname}`);
+    let responseParse = await response.json();
+
+    if (responseParse.code == 1) {
+        if (!responseParse.data == false) {
+            valid.nickname.state = false;
+            $("#nickname-error").show();
+        } else {
+            valid.nickname.state = true;
+        }
+    } else {
+        alert(responseParse.msg);
+    }
+}
+
+async function usernameSameCheck() {
+    let username = $("#username").val();
+
+    let response = await fetch(`/api/user/username-same-check?username=${username}`);
+    let responseParse = await response.json();
+
+    if (responseParse.code == 1) {
+        if (!responseParse.data == false) {
+            valid.username.state = false;
+            $("#username-error").show();
+        } else {
+            valid.username.state = true;
+        }
+    } else {
+        alert(responseParse.msg);
+    }
+}
+
+function passwordSameCheck() {
+    let password = $("#password").val();
+    let samePassword = $("#same-password").val();
+    if (password === samePassword) {
+        valid.password.state = true;
+    } else {
+        valid.password.state = false;
+        $("#password-error").show();
+    }
+} 
+
