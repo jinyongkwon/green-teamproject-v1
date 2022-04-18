b6219a4c67c95b72d7ad93ef7f219882ab8d8813
yuseunghyun <ysh7091@gmail.com>
2022-04-18 16:03:30 +0900

    [유승현] 아이디 찾기, 비밀번호 찾기 이메일 전송 로직 완료

---
+++ src/main/java/site/metacoding/greenrandomrpg/web/dto/user/UsernameRespDto.java
@@ -0,0 +1,12 @@
+package site.metacoding.greenrandomrpg.web.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UsernameRespDto {
+    private String email;
+}
